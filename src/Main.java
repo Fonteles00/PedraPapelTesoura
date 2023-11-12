e092dd6690a08ef0ae560d11badd4cd6a5fcff9c
Davi Fonteles <davifonteles3@gmail.com>
2023-11-12 19:04:28 -0300

    Código Completo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                pedraPapelTesouraGUI pedraPapelTesouraGUI = new pedraPapelTesouraGUI();
+
+                pedraPapelTesouraGUI.setVisible(true);
+            }
+        });
     }
 }
---
+++ src/pedraPapelTesoura.java
@@ -0,0 +1,81 @@
+import java.util.Random;
+
+// backend
+public class pedraPapelTesoura {
+    private static final String[] computerChoices = {"Rock", "Paper", "Scissors"};
+
+    public String getComputerChoice() {
+        return computerChoice;
+    }
+
+    public int getComputerScore() {
+        return computerScore;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    private String computerChoice;
+
+    private int computerScore, playerScore;
+
+    private Random random;
+
+    public pedraPapelTesoura(){
+        random = new Random();
+    }
+
+
+    public String playpedraPapelTesoura(String playerChoice){
+        computerChoice = computerChoices[random.nextInt(computerChoices.length)];
+
+        String result;
+
+        if(computerChoice.equals("Rock")){
+            if(playerChoice.equals("Paper")){
+                result = "Player Wins";
+                playerScore++;
+            }else if(playerChoice.equals("Scissors")){
+                result = "Computer Wins";
+                computerScore++;
+            }else{
+                result = "Draw";
+            }
+        }else if(computerChoice.equals("Paper")){
+            if(playerChoice.equals("Scissors")){
+                result = "Player Wins";
+                playerScore++;
+            }else if(playerChoice.equals("Rock")){
+                result = "Computer Wins";
+                computerScore++;
+            }else{
+                result = "Draw";
+            }
+        }else{
+            if(playerChoice.equals("Rock")){
+                result = "Player Wins";
+                playerScore++;
+            }else if(playerChoice.equals("Paper")){
+                result = "Computer Wins";
+                computerScore++;
+            }else{
+                result = "Draw";
+            }
+        }
+
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
